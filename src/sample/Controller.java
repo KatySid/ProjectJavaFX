faeffb8c930c20e62d0cb6bced82279f5b7c5c35
KatySid <whitekaty666@gmail.com>
2020-12-31 02:40:23 +0300

    homework

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    private TextArea textArea;
+
+    public void clickBtn() {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+    public void onKeyPressed(KeyEvent keyEvent) {
+        if(keyEvent.getCode()== KeyCode.ENTER) {
+            clickBtn();
+        }
+    }
 }
